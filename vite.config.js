26b452505ac53532a96f2680f494d0bb6e1395f5
white_night <yash91471@gmail.com>
2025-03-25 11:58:36 +0530

    Updated vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/portfolio/",  // Set this to your repository name
+  base: "/portfolio/",  
   plugins: [react()],
 });
